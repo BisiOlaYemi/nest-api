6081dead402eb2f3fa439e378b7b4145e34ca1fa
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-22 09:20:58 -0700

    Commiting

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './tasks/task.entity';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite3',
+      entities: [Task],
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
+})
+export class AppModule {}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  async findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
+
+  @Post()
+  async create(@Body() task: Task): Promise<Task> {
+    return this.tasksService.create(task);
+  }
+}
